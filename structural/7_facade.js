d2d3b36b311153ae8d5466997b403b97c02cda0c
murash4 <singleant@gmail.com>
2021-01-10 16:23:17 +0300

    facade

---
+++ structural/7_facade.js
@@ -0,0 +1,43 @@
+class Complaints {
+	constructor () {
+		this.complaints = []
+	}
+
+	reply (complaints) {}
+
+	add (complaint) {
+		this.complaints.push(complaint)
+		return this.reply(complaint)
+	}
+}
+
+class ProductComplaints extends Complaints {
+	reply ({ id, customer, details }) {
+		return `Product: ${id}: ${customer} (${details})`
+	}
+}
+
+class ServiceComplaints extends Complaints {
+	reply ({ id, customer, details }) {
+		return `Service: ${id}: ${customer} (${details})`
+	}
+}
+
+class ComplaintRegistry {
+	register (customer, type, details) {
+		const id = Date.now()
+		let complaint
+
+		if (type === 'service') {
+			complaint = new ServiceComplaints()
+		} else {
+			complaint = new ProductComplaints()
+		}
+
+		return complaint.add({ id, customer, details })
+	}
+}
+
+const registry = new ComplaintRegistry()
+console.log(registry.register('vlad', 'service', 'недоступен'))
+console.log(registry.register('elena', 'product', 'ошибка'))
